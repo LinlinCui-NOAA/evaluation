5bebb25b7388abb4cbf529aaa6eaf5fbc693f143
Linlin Cui <linlin.cui@noaa.gov>
2025-12-30 02:47:08 +0000

    update plots

--- aigfsv2.1/web/track/copy_file.sh
+++ aigfsv2.1/web/track/copy_file.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for id in 06 07 08 09 10 11;do
-  filename=`ls ep${id}/evs.hurricane_global_det.abstk_err.ep.2025.*.png`
+basin=al
+for id in 01 02 03 04 05 06;do
+  filename=`ls ${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.*.png`
   name=$(echo "$filename" | sed -E 's/.*2025\.([A-Z]+)[0-9]+\.png/\1/') 
   lower_name="${name,,}"
   final_name="${lower_name^}"
   echo $final_name
-  cp ep${id}/evs.hurricane_global_det.abstk_err.ep.2025.${name}${id}.png tracks_${final_name}.t.png
-  cp ep${id}/evs.hurricane_global_det.abswind_err.ep.2025.${name}${id}.png tracks_${final_name}.t.png
+  cp ${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.${name}${id}.png tracks_${final_name}.t.png
+  cp ${basin}${id}/evs.hurricane_global_det.abswind_err.${basin}.2025.${name}${id}.png tracks_${final_name}.i.png
 
 
 done
