52bfa2f714da4d56827a19c6a53e4a765006a9ad
Linlin Cui <linlin.cui@noaa.gov>
2025-12-31 21:45:56 +0000

    update track files

--- aigfsv2.1/web/track/copy_file.sh
+++ aigfsv2.1/web/track/copy_file.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-basin=al
-for id in 01 02 03 04 05 06;do
-  filename=`ls ${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.*.png`
-  name=$(echo "$filename" | sed -E 's/.*2025\.([A-Z]+)[0-9]+\.png/\1/') 
-  lower_name="${name,,}"
-  final_name="${lower_name^}"
-  echo $final_name
-  cp ${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.${name}${id}.png tracks_${final_name}.t.png
-  cp ${basin}${id}/evs.hurricane_global_det.abswind_err.${basin}.2025.${name}${id}.png tracks_${final_name}.i.png
+path=/scratch3/NAGAPE/gpu-ai4wp/Linlin.Cui/VRFY/evs/aigfsv2.1
+basin=wp
 
+#for id in `seq -w 1 13`;do
+#  filename=`ls ${path}/${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.*.png`
+#  name=$(echo "$filename" | sed -E 's/.*2025\.([A-Z]+)[0-9]+\.png/\1/') 
+#  lower_name="${name,,}"
+#  final_name="${lower_name^}"
+#  echo $final_name
+#  rm tracks_${final_name}.t.png
+#  #ls ${path}/${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.${name}${id}.png
+#  cp ${path}/${basin}${id}/evs.hurricane_global_det.abstk_err.${basin}.2025.${name}${id}.png tracks_${final_name}.t.png
+#
+#  rm tracks_${final_name}.i.png
+#  cp ${path}/${basin}${id}/evs.hurricane_global_det.abswind_err.${basin}.2025.${name}${id}.png tracks_${final_name}.i.png
+#
+#
+#done
 
-done
+#fullname=Atlantic
+#fullname=EastPacific
+fullname=WestPacific
+rm tracks_${basin}.t.png
+ls ${path}/${fullname}/evs.hurricane_global_det.abstk_err.${basin}.2025.${name}${id}season.png
+cp ${path}/${fullname}/evs.hurricane_global_det.abstk_err.${basin}.2025.${name}${id}season.png tracks_${basin}.t.png
+
+rm tracks_${basin}.i.png
+ls ${path}/${fullname}/evs.hurricane_global_det.abswind_err.${basin}.2025.${name}${id}season.png
+cp ${path}/${fullname}/evs.hurricane_global_det.abswind_err.${basin}.2025.${name}${id}season.png tracks_${basin}.i.png
